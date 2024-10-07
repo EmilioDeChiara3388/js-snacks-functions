9f7dd9f08ee9bcb31ce6e3724d81cee2406630df
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-07 18:00:45 +0200

    add: saluti

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,22 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function countVowels (word){
+    for (let i = 0; i <= word.length; i++){
+        console.log(i);
+        let count = 0;
+        if (i === "a" && i === "e" && i === "i" && i === "o" && i === "u"){
+            count++;
+        }
+        
+    }
 
+    return count;
+
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(countVowels(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -4,10 +4,15 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+function yourName(word){
+    let greetings = `${"Ciao"} ${word}`;
+    return greetings;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(yourName("Mario"));
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
